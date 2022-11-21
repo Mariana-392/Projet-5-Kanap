5aa855de53c37bf532ce92120055aaed88e2b7d6
Mariana <de_sousa_mariana@msn.com>
2022-11-21 17:27:45 +0100

    requête API + insertion produits page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,62 @@
+//requêter l'API 
+console.log('Hello');
+function getAllProducts(){
+    console.log('Bonjour');
+    fetch ("http://localhost:3000/api/products")
+        .then(function(res) {
+            console.log(res);
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function(value) {
+            showProducts(value);
+            console.log(value);
+        })
+        .catch(function(err) {
+        // Une erreur est survenue
+        });
+}
+function showProducts(products){
+    products.forEach(function(product){
+        const itemSection= document.getElementById('items');
+
+        //création des balises
+        const a = document.createElement("a");
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        const h3 = document.createElement("h3");
+        const p = document.createElement("p");
+
+        //lie les balises au contenu de l'API
+        img.src = product.imageUrl;
+        h3.innerText = product.name;
+        p.innerText = product.description;
+
+        //ajout attribut et class
+        a.setAttribute("href", "./product.html?id=" + showProducts._id);
+        img.setAttribute("alt", product.altTxt);
+        h3.classList.add("productName");
+        p.classList.add("productDescription");
+
+
+        //ajout à la page web
+        itemSection.appendChild(a);
+        a.appendChild(article);
+        article.appendChild(img);
+        article.appendChild(h3);
+        article.appendChild(p);
+    });
+}
+
+getAllProducts();
+
+//Dire où va les produits dans html
+//let elt = document.getElementById('items');
+/* Créer les balises a, article, img, h3 et p => var ** = document.createElement ('')?
+elt.innerHTML = ""; =>pour ajouter le contenu au html 
+ajouter href à a =
+ajouter img et le alt =
+ajouter class aux balises h3 et p =
+
+const new */
